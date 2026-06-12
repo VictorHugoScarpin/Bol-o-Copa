03d51a92dd7148c8663c9618545319b9b8a0ac83
Scarpin_Xavier <vhsxtrabalho@gmail.com>
2026-06-11 21:48:21 -0300

    Update MatchesPage.jsx

--- src/pages/MatchesPage.jsx
+++ src/pages/MatchesPage.jsx
@@ -84,7 +84,7 @@ const TEAM_PT = {
   'Bosna i Hercegovina': 'Bósnia e Herzegovina',
 }
 
-// Brasões fixos por selesção (Wikipedia)
+// Brasões fixos por seleção (Wikipedia)
 const TEAM_SHIELD = {
   'United States': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/United_States_Soccer_Federation_logo.svg/200px-United_States_Soccer_Federation_logo.svg.png',
   'USA': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/United_States_Soccer_Federation_logo.svg/200px-United_States_Soccer_Federation_logo.svg.png',
